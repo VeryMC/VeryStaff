481accb5b32edcc28d6a4a0fb051b66d7b7c80db
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 13:03:14 +0100

    Fix

--- src/main/java/fr/verymc/manager/InventoryManager.java
+++ src/main/java/fr/verymc/manager/InventoryManager.java
@@ -6,6 +6,7 @@ import fr.verymc.Listener.ListenerEvent;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
